a07c69323614cb65581fae0d383fe6ac2080a13f
punkerisaacs <ricardo.lopez@cpavs.mx>
2020-08-21 22:31:51 -0500

    config react, typescript and webpack

---
+++ src/index.tsx
@@ -0,0 +1,4 @@
+import * as React from 'react'
+import * as reactDom from 'react-dom'
+
+reactDom.render(<h1>hola dddddd</h1>, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+    entry: {
+        app: path.join(__dirname, 'src', 'index.tsx')
+    },
+    target: "web",
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx']
+    },
+    output: {
+        filename: 'app.[hash].js',
+        path: path.join(__dirname, 'dist')
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: "public/index.html"
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.tsx$/,
+                exclude: /node_modules/,
+                use: 'ts-loader'
+            }
+        ]
+    }
+}
